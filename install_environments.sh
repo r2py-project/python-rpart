270fe0cc67f6e0f066eb460892fa9610510356d8
Yufei Cai <248735861+caiyufei8@users.noreply.github.com>
2026-06-08 15:40:48 -0400

    Create test env.

---
+++ install_environments.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#$ -pe smp 1
+#$ -q long
+# conda create -n r-to-python python r-base -c conda-forge -y
+module load gcc
+conda activate r-to-python
+conda install -c conda-forge meson-python numpy pandas scipy scikit-learn matplotlib r-kernsmooth -y
+conda install -c conda-forge r-cardata -y
+conda install -c conda-forge rpy2 -y
+conda install -c conda-forge radian -y
+R
+install.packages("vscDebugger", repos = c("https://manuelhentschel.r-universe.dev", "https://cloud.r-project.org"))
