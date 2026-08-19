104caa18fe25aa4c53ef8840135e83b2d15e7822
Yufei Cai <248735861+caiyufei8@users.noreply.github.com>
2026-08-19 11:52:04 -0400

    Correct scripts

--- git_pull.sh
+++ git_pull.sh
@@ -3,7 +3,7 @@
 #$ -q long
 module load git
 export PATH=~/bin:$PATH
-# git remove add python-rpart https://github.com/r2py-project/python-rpart.git
+# git remote add python-rpart https://github.com/r2py-project/python-rpart.git
 git pull origin main
 # git remote add r2py_rpart https://github.com/r2py-project/r2py_rpart.git
 git subtree pull --squash --prefix=r2py_rpart r2py_rpart main
--- git_push.sh
+++ git_push.sh
@@ -3,7 +3,7 @@
 #$ -q long
 module load git
 export PATH=~/bin:$PATH
-# git remove add python-rpart https://github.com/r2py-project/python-rpart.git
+# git remote add python-rpart https://github.com/r2py-project/python-rpart.git
 git push origin main
 # git remote add r2py_rpart https://github.com/r2py-project/r2py_rpart.git
 git subtree push --prefix=r2py_rpart r2py_rpart main
